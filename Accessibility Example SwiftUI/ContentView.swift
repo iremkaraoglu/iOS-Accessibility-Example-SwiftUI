fc91ff7a9ceda58e37e48a1b52d513d090179eb2
iremkaraoglu <iremk97@gmail.com>
2021-11-01 19:50:22 +0300

    create initial screen and add assets

--- Accessibility Example SwiftUI/ContentView.swift
+++ Accessibility Example SwiftUI/ContentView.swift
@@ -9,8 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            Text("Get to know pandas 🐼")
+            Image("panda")
+                .resizable()
+                .frame(width: 300.0, height: 400.0)
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Start learning").padding()
+                    
+            }
+            
+        }
+        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height + 2)
+        .background(Color.init(UIColor(red: 173/255, green: 217/255, blue: 185/255, alpha: 1.0)))
     }
 }
 
