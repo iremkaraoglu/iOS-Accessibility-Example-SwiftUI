252aeb16183ae562ebd974add3d024d3ca722ffe
iremkaraoglu <iremk97@gmail.com>
2021-11-01 10:11:58 +0300

    Initial Commit

---
+++ Accessibility Example SwiftUI/Accessibility_Example_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accessibility_Example_SwiftUIApp.swift
+//  Accessibility Example SwiftUI
+//
+//  Created by Irem Karaoglu on 1.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Accessibility_Example_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Accessibility Example SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Accessibility Example SwiftUI
+//
+//  Created by Irem Karaoglu on 1.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
