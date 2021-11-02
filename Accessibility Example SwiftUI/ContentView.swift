4b31ebdad7d395abb9d45e3dee9e3e2385de55ff
iremkaraoglu <iremk97@gmail.com>
2021-11-02 14:14:00 +0300

    improve ui

--- Accessibility Example SwiftUI/ContentView.swift
+++ Accessibility Example SwiftUI/ContentView.swift
@@ -9,19 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        NavigationView{
             ZStack{
                 Color(UIColor(red: 173/255, green: 217/255, blue: 185/255, alpha: 1.0))
                 VStack {
                     Text("Get to know pandas 🐼")
+                        .font(.system(size: 30))
+                        .bold()
                     Image("panda")
                         .resizable()
                         .frame(width: 300.0, height: 400.0)
+                        .border(Color.black)
+                        .cornerRadius(30)
                     Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                        Text("Start learning").padding()
-                    }
+                        Text("Start learning")
+                            .foregroundColor(Color.white)
+                    }.padding()
+                        .background(Color(red: 0, green: 0, blue: 0))
+                        .clipShape(Capsule())
                 }
             }.ignoresSafeArea(.all)
-        
+        }
     }
 }
 
